bc027b998439ea0cf542e516c05097fb88d87c44
Samuel Mandel <slman.760@gmail.com>
2022-05-09 23:31:46 -0700

    add to dashboard and homepage for handlebar, post route to create blog made

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,4 +1,14 @@
-// const router = require('express').Router();
-// const { Blog } = require('../../models');
+const router = require('express').Router();
+const { Blog } = require('../../models');
 
-// router.post('/')
+router.post('/', withAuth, async (req, res)=> {
+    try{
+        const newBlog = await Blog.create({
+            ...req.body, 
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newBlog);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-//add helpers if used
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({})
+const hbs = exphbs.create({helpers})
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
