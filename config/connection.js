9cc877c779ac3937817dd2a8d079b6da4a5118ac
Samuel Mandel <slman.760@gmail.com>
2022-05-09 21:47:15 -0700

    jawsDB for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+let sequelize;
 
-const sequelize = new Sequelize(
+if(process.env.JAWSDB_URL){
+ sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+ sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
